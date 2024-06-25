375f9d934733a5b182a25a677b92f748947ab904
mandeepkakkar2003 <mandeepkakkar2003@gmail.com>
2024-06-25 14:35:15 +0530

    Updated baseURL to new backend URL

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const emailForm = document.querySelector("#emailForm");
 
 const toast = document.querySelector(".toast");
 
-const baseURL = "https://fileshairing-4.onrender.com"; // Updated base URL
+const baseURL = "https://filesharing-drzs.onrender.com"; // Updated base URL
 const uploadURL = `${baseURL}/api/files`;
 const emailURL = `${baseURL}/api/files/send`;
 
